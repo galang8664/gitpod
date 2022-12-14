1b7fa689124049dd5132c5ab9171bc562d56d202
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2022-12-14 20:10:56 -0300

    1

--- components/ws-manager/pkg/manager/manager.go
+++ components/ws-manager/pkg/manager/manager.go
@@ -1739,8 +1739,8 @@ func waitForSecretInNamespace(client client.Client, namespace, name string) erro
 
 func secretInNamespace(client client.Client, namespace, name string) wait.ConditionFunc {
 	return func() (bool, error) {
-		var secret *corev1.Secret
-		err := client.Get(context.TODO(), types.NamespacedName{Namespace: namespace, Name: name}, secret)
+		var secret corev1.Secret
+		err := client.Get(context.TODO(), types.NamespacedName{Namespace: namespace, Name: name}, &secret)
 		if k8serr.IsNotFound(err) {
 			return false, nil
 		}
@@ -1749,10 +1749,6 @@ func secretInNamespace(client client.Client, namespace, name string) wait.Condit
 			return false, err
 		}
 
-		if secret != nil {
-			return true, nil
-		}
-
-		return false, nil
+		return true, nil
 	}
 }
